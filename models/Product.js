64b6896047c9d556e7cb7018ae00732114209c88
harirajhari <hariraj121203@gmail.com>
2025-06-06 13:56:55 +0530

    time stamp created

--- models/Product.js
+++ models/Product.js
@@ -1,18 +1,23 @@
 // models/Product.js
 const mongoose = require('mongoose');
 
-const productSchema = new mongoose.Schema({
-  productName: { type: String, required: true },
-  details: { type: String, required: true },
-  stock: { type: Number, required: true },
-  discountPrice: { type: Number },
-  originalPrice: { type: Number, required: true },
-  category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category' },
-  subCategory: { type: mongoose.Schema.Types.ObjectId, ref: 'SubCategory' },
-  status: { type: String, required: true },
-  sold: { type: Number, default: 0 },
-  material: {type: String, required: true},
-  productImages: [{ type: mongoose.Schema.Types.ObjectId, ref: 'ProductImage' }]
-});
+const productSchema = new mongoose.Schema(
+  {
+    productName: { type: String, required: true },
+    details: { type: String, required: true },
+    stock: { type: Number, required: true },
+    discountPrice: { type: Number },
+    originalPrice: { type: Number, required: true },
+    category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category' },
+    subCategory: { type: mongoose.Schema.Types.ObjectId, ref: 'SubCategory' },
+    status: { type: String, required: true },
+    sold: { type: Number, default: 0 },
+    material: { type: String, required: true },
+    productImages: [{ type: mongoose.Schema.Types.ObjectId, ref: 'ProductImage' }],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model('Product', productSchema);
